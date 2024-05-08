d4ab107db120d4c6c0a55ac0a2a5e5ceb6355577
Laércio Soares Fragoso <laerciosoaresfragoso@gmail.com>
2024-05-08 11:23:26 -0700

    Operadores Lógicos

---
+++ Aula3-OperadoresLogicos/MyPlayground.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+//Incrementos, Operadores Lógicos
+
+let res1 = 2 > 4
+
+print(res1)
+
+let x = 1
+let y = 6
+let res2 = x == y
+let res3 = x != y
+
+print(res2)
+
+print(res3)
+
+
+var pontuacao1 = 8.0
+var pontuacao2 = 10.0
+
+// comparando as pontuações
+print(pontuacao1 > pontuacao2) // false
+print(pontuacao1 >= pontuacao2) // false
+print(pontuacao1 < pontuacao2) // true
+print(pontuacao1 <= pontuacao2) // true
+print(pontuacao1 == pontuacao2) // false
+print(pontuacao1 != pontuacao2) // true
+
+/*
+ Os operadores lógicos são utilizados para combinar expressões e determinar a verdade ou falsidade de uma condição composta.
+ Os principais operadores lógicos são:
+ 
+ “AND” (&&)
+ “OR” (||)
+ “NOT” (!)
+ 
+ */
