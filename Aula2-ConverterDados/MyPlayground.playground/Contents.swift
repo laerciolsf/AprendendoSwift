7e8d213d7a8958479f120b3ae68e87b05be2f903
Laércio Soares Fragoso <laerciosoaresfragoso@gmail.com>
2024-05-08 09:29:09 -0700

    Converter Dados (Casting)

---
+++ Aula2-ConverterDados/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//Converter dados (Casting).
+
+let x = 10
+let y = String(x)
+
+print(y)
+
+
+let a = "15"
+let b = Int(a)
+
+print(b) // No caso da String ser uma letra ele retorna um null.
