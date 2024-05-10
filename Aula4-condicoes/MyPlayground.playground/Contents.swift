acf66fbe1e541fa189a4b4c9650a6f55bda2dc09
Laércio Soares Fragoso <laerciosoaresfragoso@gmail.com>
2024-05-10 10:53:33 -0700

    Condições

---
+++ Aula4-condicoes/MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+
+import UIKit
+
+// condiçoes
+
+let estaComFome = true
+
+
+if estaComFome{
+    print("Estou com fome!")
+}else{
+    print("Não estou como fome!")
+}
+
+
+let teamOneScore = 7
+let teamTwoScore = 6
+
+if teamOneScore > teamTwoScore {
+    print("Team One Won")
+} else if teamTwoScore > teamOneScore {
+    print("Team Two Won")
+} else {
+    print("We have a tie")
+}
